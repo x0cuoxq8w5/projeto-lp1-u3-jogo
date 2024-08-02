6ca2d86a99db3175bf83a0719d7863b3b4c34695
x0cuoxq8w5 <artur.nobre.114@ufrn.edu.br>
2024-08-02 14:16:50 -0300

    Player desbugado e resolução reduzida

--- main.cpp
+++ main.cpp
@@ -8,7 +8,8 @@ using namespace sf;
 // Ideia pra conseguir o resto da pontuação: Implementar um menu de upgrade acessível apertando ESC
 // Nesse menu você poderia talvez aumentar o seu hp máximo, hp máximo da base e botar pra base regenerar
 const float raiosprite = 16.f;
-const Vector2f centrotela = {960, 540};
+//pra 1080p, 960x540
+const Vector2f centrotela = {800, 450};
 float calculaEscalar(Vector2f vetor) {
     return sqrt((vetor.x*vetor.x)+(vetor.y*vetor.y)); //nota para si: não esqueça da raiz quadrada do escalar
 }
@@ -18,17 +19,18 @@ class Player {
     int dinheiro;
     int muni;
 public:
+    Window *janela;
     float movespeed;
     CircleShape sprite;
     Vector2f velocidade;
     Vector2f alvo;
     Vector2f versoralvo; //e foi no exato momento que percebi que eu ia precisar de versor pra fazer isso de uma forma bacana
     //que eu me senti genial por fazer VGA
-    Player(float posX, float posY){
+    Player(float posX, float posY, Window *vindow){
+        janela = vindow;
         hp = 100;
         muni = 100;
         dinheiro = 0;
-        alvo = centrotela;
         velocidade = {0,0};
         versoralvo = {0,0};
         movespeed = 10;
@@ -38,17 +40,24 @@ public:
         sprite.setOutlineThickness(4);
         sprite.setFillColor(Color::White);
         sprite.setOrigin(raiosprite, raiosprite);
+        alvo = sprite.getPosition();
     }
     void update(){
-        sprite.move(velocidade);
         if(Mouse::isButtonPressed(sf::Mouse::Right)) {
-            alvo = (Vector2f)Mouse::getPosition() - sprite.getPosition(); //casting pra V2f por motivos de o Mouse::getPosition retornar V2i
-            /*cout << "Rightclick! Mousepos: " << Mouse::getPosition().x << ", " << Mouse::getPosition().y << "\nSpritepos: "
-            << sprite.getPosition().x << ", " << sprite.getPosition().y << "\nAlvo: " << alvo.x << ", " << alvo.y << endl;*/
+            alvo = (Vector2f)Mouse::getPosition(*janela) - sprite.getPosition(); //casting pra V2f por motivos de o Mouse::getPosition retornar V2i
         }
-        versoralvo = alvo/calculaEscalar(alvo);
-        velocidade = versoralvo*movespeed;
-        alvo -= velocidade; //por algum motivo o player fica flickering quando ele chega na posição alvo.
+        if ((alvo.x != 0) && (alvo.y != 0)) {
+            versoralvo = alvo / calculaEscalar(alvo);
+            velocidade = versoralvo * movespeed;
+            if (calculaEscalar(alvo) < calculaEscalar(velocidade)) {
+                velocidade = alvo;
+            }
+            alvo -= velocidade;
+        }
+        else { velocidade = {0,0};}
+        /*cout << "Spritepos: " << sprite.getPosition().x << ", " << sprite.getPosition().y << "\nAlvo: " << alvo.x << ", "
+        << alvo.y << "\nVelocidade: " << velocidade.x << ", " << velocidade.y << endl;;*/
+        sprite.move(velocidade);
     }
     void atirar(){
     }
@@ -72,11 +81,12 @@ class Inimigo{
 
 
 int main() {
-    Player player(centrotela.x, centrotela.y);
 
-    auto window = sf::RenderWindow{ { 1920u, 1080u }, "Amo videogames" };
+    auto window = sf::RenderWindow{ { 1600u, 900u }, "Amo videogames"};
+    //algum de vocês por favor busque depois fazer com que se você botar maximizado ele ainda vá pra onde o mouse está
+    //provavelmente vai requerer catucar com o sf::Event::Resized
     window.setFramerateLimit(60 );
-
+    Player player(centrotela.x, centrotela.y, &window);
     while (window.isOpen())
     {
         for (auto event = sf::Event{}; window.pollEvent(event);)
