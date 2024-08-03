23b585ca68ba3856377ab2cc6282ff237d267ea9
x0cuoxq8w5 <artur.nobre.114@ufrn.edu.br>
2024-08-03 20:58:43 -0300

    Projeteis funcionais, esqueleto da base.

--- main.cpp
+++ main.cpp
@@ -28,31 +28,39 @@ public:
     //que eu me senti genial por fazer VGA
 };
 class Projetil : public Bola {
-    float movespeed = 50;
+    float movespeed = 10;
+    int time; //será utilizado depois para propósitos de colisão; 0 = player, 1 = inimigo
 public:
-    Projetil(Vector2f target, Vector2f pos){
+    bool clrflag = false;
+    Projetil(Vector2f target, Vector2f pos, int team){
         versoralvo = calculaVersor(target-pos);
         velocidade = versoralvo*movespeed;
         sprite.setPosition(pos);
         sprite.setRadius(raioprojetil);
         sprite.setFillColor(Color::Red);
         sprite.setOrigin(raioprojetil, raioprojetil);
+        time = team;
     };
     void update(){
         sprite.move(velocidade);
         if (sprite.getPosition().x < 0 || sprite.getPosition().x > dimensoestela.x || sprite.getPosition().y < 0 || sprite.getPosition().y > dimensoestela.y) {
-            delete this;
+            clrflag = true;
         }
     };
 };
-class Player : public Bola{
+class Shooter : public Bola{
+protected:
+    vector<Projetil>* projeteisaddr;
+};
+class Player : public Shooter{
     int hp;
     int dinheiro;
     int muni;
 public:
     Window *janela;
-    Player(float posX, float posY, Window *vindow){
+    Player(float posX, float posY, Window *vindow, vector<Projetil> *proj){
         janela = vindow;
+        projeteisaddr = proj;
         hp = 100;
         muni = 100;
         dinheiro = 0;
@@ -71,6 +79,14 @@ public:
         if(Mouse::isButtonPressed(sf::Mouse::Right)) {
             alvo = (Vector2f)Mouse::getPosition(*janela) - sprite.getPosition(); //casting pra V2f por motivos de o Mouse::getPosition retornar V2i
         }
+        calculoMovimento();
+        sprite.move(velocidade);
+    }
+    void atirar(){
+        projeteisaddr->emplace_back((Vector2f)Mouse::getPosition(*janela), sprite.getPosition(), 0);
+        //aparentemente nas versões mais novas do C++ tem uma função pra vetor chamada emplace_back que faz o mesmo que push_back([typename](argumentos))
+    }
+    void calculoMovimento(){//Joguei o código pra dentro duma função pra ficar mais fácil de ler
         if ((alvo.x != 0) || (alvo.y != 0)) {
             versoralvo = alvo / calculaEscalar(alvo);
             velocidade = versoralvo * movespeed;
@@ -79,11 +95,6 @@ public:
             alvo -= velocidade;
         }
         else { velocidade = {0,0};}
-        /*cout << "Spritepos: " << sprite.getPosition().x << ", " << sprite.getPosition().y << "\nAlvo: " << alvo.x << ", "
-        << alvo.y << "\nVelocidade: " << velocidade.x << ", " << velocidade.y << endl;;*/
-        sprite.move(velocidade);
-    }
-    void atirar(){
     }
     void levardano(){
     }
@@ -94,7 +105,7 @@ public:
     float baixo() { return y() + sprite.getPosition().y / 2.f; };
     float esquerda() { return x() - sprite.getPosition().x / 2.f; };
 };
-class Inimigo : public Bola{
+class Inimigo : public Shooter{
     Inimigo(){};
     void update(){
         sprite.move(velocidade);
@@ -102,7 +113,24 @@ class Inimigo : public Bola{
     void morrer(){
     };
 };
-class Base{};
+class Base{
+    int hp;
+private:
+    void levardano(){};
+public:
+    RectangleShape sprite;
+    void update(){
+        //colisão vai aqui
+    }
+    Base(){
+        sprite.setFillColor(Color::Black);
+        sprite.setOutlineColor(Color::Blue);
+        sprite.setOutlineThickness(10);
+        sprite.setSize({250,100});
+        sprite.setPosition(centrotela);
+        sprite.setOrigin({125,50});
+    }
+};
 
 
 
@@ -113,7 +141,10 @@ int main() {
     //algum de vocês por favor busque depois fazer com que se você botar maximizado ele ainda vá pra onde o mouse está
     //provavelmente vai requerer catucar com o sf::Event::Resized
     window.setFramerateLimit(60 );
-    Player player(centrotela.x, centrotela.y, &window);
+    window.setKeyRepeatEnabled(false);
+    vector<Projetil> projeteis;
+    Player player(centrotela.x, centrotela.y, &window, &projeteis);
+    Base base;
     while (window.isOpen())
     {
         for (auto event = sf::Event{}; window.pollEvent(event);)
@@ -122,10 +153,26 @@ int main() {
             {
                 window.close();
             }
+            if (event.type == Event::KeyPressed) {
+                if(event.key.code == Keyboard::Q) {
+                    player.atirar();
+                }
+            }
         }
         player.update();
+        for (Projetil & proj : projeteis) {
+            proj.update();
+        }
+        projeteis.erase(std::remove_if(projeteis.begin(), projeteis.end(), [&](const Projetil &item) {
+            return item.clrflag;
+        }), projeteis.end()); //levemente gambiarrento mas pelo que entendi o remove if mata o elemento e joga pro final do vetor
+        //e o erase apaga
         window.clear();
+        window.draw(base.sprite);
         window.draw(player.sprite);
+        for (Projetil & proj : projeteis) {
+            window.draw(proj.sprite);
+        }
         window.display();
     }
 }
